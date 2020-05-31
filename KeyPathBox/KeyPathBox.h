f5db0f82888f48ee1ed8c49a5708ff2d39696971
joshua.rhyno <joshua.rhyno@kakaocorp.com>
2020-05-31 10:57:54 +0900

    Initial Commit

---
+++ KeyPathBox/KeyPathBox.h
@@ -0,0 +1,19 @@
+//
+//  KeyPathBox.h
+//  KeyPathBox
+//
+//  Created by Joshua on 2020/05/31.
+//  Copyright © 2020 Joshua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KeyPathBox.
+FOUNDATION_EXPORT double KeyPathBoxVersionNumber;
+
+//! Project version string for KeyPathBox.
+FOUNDATION_EXPORT const unsigned char KeyPathBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyPathBox/PublicHeader.h>
+
+
